b0cc783bb71ca31a6ef569a4e837d800f4ad058b
Prajyoth <prajyoth.p.k@gmail.com>
2025-10-03 15:02:42 +0000

    Batman_Topo

--- mininet/topology.py
+++ mininet/topology.py
@@ -1,33 +1,45 @@
 #!/usr/bin/env python
 
 from mininet.net import Mininet
-from mininet.node import Controller, RemoteController
+from mininet.node import RemoteController, OVSKernelSwitch
 from mininet.cli import CLI
 from mininet.log import setLogLevel, info
 from mininet.link import TCLink
+from mininet.topo import Topo
 
-def myNetwork():
-    info('*** Adding controller\n')
-    # Make sure this IP is correct for your VM
-    c0 = RemoteController('c0', ip='192.168.64.4', port=6633)
+class MyTopo(Topo):
+    def build(self):
+        # Create switches
+        switches = []
+        for i in range(1, 7):
+            s = self.addSwitch(f's{i}', cls=OVSKernelSwitch, protocols='OpenFlow13')
+            switches.append(s)
 
-    info('*** Creating a network\n')
-    net = Mininet(controller=c0, link=TCLink) 
+        # Create hosts
+        hosts = []
+        for i in range(1, 19):
+            h = self.addHost(f'h{i}', ip=f'10.0.0.{i}/24', 
+                             mac=f'00:00:00:00:00:{i:02x}', cpu=1.0/20)
+            hosts.append(h)
 
-    info('*** Adding hosts and switch\n')
-    h1 = net.addHost('h1', ip='10.0.0.1/24')
-    h2 = net.addHost('h2', ip='10.0.0.2/24')
-    s1 = net.addSwitch('s1')
+        # Connect hosts to switches (3 per switch)
+        for i in range(6):
+            for j in range(3):
+                self.addLink(hosts[i*3 + j], switches[i])
 
-    info('*** Creating links\n')
-    net.addLink(h1, s1)
-    net.addLink(h2, s1)
+        # Connect switches in a linear chain
+        for i in range(5):
+            self.addLink(switches[i], switches[i+1])
+
+def startNetwork():
+    info('*** Creating network\n')
+    topo = MyTopo()
+    # Make sure this IP is your controller's IP
+    c0 = RemoteController('c0', ip='192.168.64.4', port=6653)
+    net = Mininet(topo=topo, link=TCLink, controller=c0)
 
     info('*** Starting network\n')
     net.start()
-    
-    info('*** Forcing switch to connect to controller\n')
-    s1.cmd('ovs-vsctl set-controller s1 tcp:192.168.64.4:6633')
 
     info('*** Running CLI\n')
     CLI(net)
@@ -37,4 +49,4 @@ def myNetwork():
 
 if __name__ == '__main__':
     setLogLevel('info')
-    myNetwork()
+    startNetwork()
